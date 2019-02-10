45267935533af65d1ba4fee7f4fa98849016b9f0
Roy Davies <roydavies209@gmail.com>
2019-02-10 21:18:26 +0000

    Feature - Enemy Movement

---
+++ LaserDefender/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour {
+
+    //config params
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+
+    //static params
+    int waypointIndex = 0;
+
+	//unity methods
+	void Start () {
+        transform.position = waypoints[waypointIndex].transform.position;
+	}
+
+	void Update () {
+        if(waypointIndex <= waypoints.Count-1) {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+            if(transform.position == targetPosition) {
+                waypointIndex++;
+            }
+        }
+	}
+
+}
--- LaserDefender/Assets/Scripts/Player.cs
+++ LaserDefender/Assets/Scripts/Player.cs
@@ -5,18 +5,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    //config params
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float padding = 0.5f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float laserSpeed = 10f;
+    [SerializeField] float projectileFiringPeriod = 0.1f;
+
+    Coroutine firingCoroutine;
+
+    //static params
     float xMin;
     float xMax;
     float yMin;
     float yMax;
 
-	// Use this for initialization
+	//unity methods
 	void Start () {
         SetUpMoveBoundaries();
 	}
 
+    void Update () {
+        Move();
+        Fire();
+	}
+
+    //custom methods
     private void SetUpMoveBoundaries() {
         Camera gameCamera = Camera.main;
         xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
@@ -26,11 +40,6 @@ public class Player : MonoBehaviour {
         yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
-    // Update is called once per frame
-    void Update () {
-        Move();
-	}
-
     private void Move() {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
@@ -39,4 +48,22 @@ public class Player : MonoBehaviour {
         var newYPosition = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newXPosition, newYPosition);
     }
+
+    private void Fire() {
+        if(Input.GetButtonDown("Fire1")) {
+            firingCoroutine = StartCoroutine(FireContinuously());
+        }
+        if(Input.GetButtonUp("Fire1")) {
+            StopCoroutine(firingCoroutine);
+        }
+    }
+    
+    IEnumerator FireContinuously() {
+        while(true) {
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, laserSpeed);
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        }
+    }
+
 }
